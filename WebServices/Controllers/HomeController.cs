3a135fad3cd75ab8f6a9035fe8ac33e95ab24683
fsprogamer <fsprogamer@gmail.com>
2016-08-19 14:40:56 +0300

    Second

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -1,31 +1,12 @@
-﻿using WebServices.Models;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-        ReservationRespository repository = ReservationRespository.Current;
         public ViewResult Index()
         {
-            return View(repository.GetAll());
-        }
-
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                repository.Add(item);
-                return RedirectToAction("Index");
-            }
-            else return View("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && repository.Update(item))
-                return RedirectToAction("Index");
-            else return View("Index");
+            return View();
         }
     }
 }
